40611aa0e531783fb99fcff01c464132f6b6a6d9
Cong Khoa Huynh <bhuynhcongkhoa@gmail.com>
2022-08-04 15:13:31 +0700

    Build Default Layout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,17 @@
+
+import clsx from 'clsx';
 import Header from '~/components/Layout/components/Header';
 import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={clsx(styles.wrapper)}>
             <Header />
-            <div className="container">
+            <div className={clsx(styles.container)}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={clsx(styles.content)}>{children}</div>
             </div>
         </div>
     );
